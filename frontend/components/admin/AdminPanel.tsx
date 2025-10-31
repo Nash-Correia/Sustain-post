806cbef1d57ea240b092a25683b6156797c0034e
Nash Correia <nash.correia@iias.in>
2025-10-31 15:06:47 +0530

    b

--- frontend/components/admin/AdminPanel.tsx
+++ frontend/components/admin/AdminPanel.tsx
@@ -435,7 +435,7 @@ const AdminPanel: React.FC = () => {
                   Clear Dates
                 </button>
               )}
-              <div className='item-justify-end'>
+              <div className='flex justify-end'>
               <button
                   onClick={handleRefreshLogs}
                   className="rounded-lg bg-gray-100 px-4 py-2 text-gray-700 transition-colors hover:bg-gray-200"
